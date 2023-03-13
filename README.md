f26dcaa1630bf6033807f11d1ed43e00cf43f7c1
Tifni Shroll <tasshroll@gmail.com>
2023-03-13 09:46:04 -0600

    added ReadMe

--- README.md
+++ README.md
@@ -1,33 +1,79 @@
-# passwords
-Application that an employee can use to generate a random password based on criteria they've selected.
+# passworddds
+This application allows a user to generate a random password based on criteria they've selected.
+
+# passworddds Webpage
+Link to Deployed Application;
+https://tasshroll.github.io/passworddds/Develop/
+
+Screenshot of application creating a password of length 90 characters with only lowercase letters.
+!(Assets/Password-screenshot.png)
+or
+https://github.com/tasshroll/passworddds/issues/1#issue-1621717123
+!(https://github.com/tasshroll/passworddds/issues/1#issue-1621717123)
 
 
-User Story
+## Description
+This Application will generate a random password based on user criterria selected.
+## User Story
+
+```
 AS AN employee with access to sensitive data
 I WANT to randomly generate a password that meets certain criteria
 SO THAT I can create a strong password that provides greater security
-Acceptance Criteria
+```
+
+## Acceptance Criteria
+
+```
 GIVEN I need a new, secure password
 WHEN I click the button to generate a password
 THEN I am presented with a series of prompts for password criteria
-“Check all that you want to apply to your new password”
-Lowercase letters a,b,c,d,e,f,g,h,I,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z
-Uppercase letters A,B,C,DD,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z
-Numeric 0,1,2,3,4,5,6,7,8,9
-Special Characters !"#$%&'()*+,-./:;<=>?@[\]^_`{|}~"
-
-Build an array with all valid choices, var [] choiceSet
-
-What length do you want for your new password (8-128?
-
-for let i=0, i<length, i++ {
-  newPswd[i] = randomPick from choiceSet;
-
-
-
 WHEN prompted for password criteria
 THEN I select which criteria to include in the password
 WHEN prompted for the length of the password
 THEN I choose a length of at least 8 characters and no more than 128 characters
 WHEN asked for character types to include in the password
 THEN I confirm whether or not to include lowercase, uppercase, numeric, and/or special characters
+WHEN I answer each prompt
+THEN my input should be validated and at least one character type should be selected
+WHEN all prompts are answered
+THEN a password is generated that matches the selected criteria
+WHEN the password is generated
+THEN the password is either displayed in an alert or written to the page
+```## User Story
+
+
+- What was your motivation? 
+To use character strings to build a valid character set based on user input.
+To use an array to build a new password
+
+- Why did you build this project? 
+This project allows user input to create output in a text area
+
+- What did you learn?
+I learned to analyze the mockup and write pseudo code to generate the password. I created several inner functions without my generate-password function to encapsulate functionality. I learned about global and local variables and event listeners. I utilized the prompt attribute to get user data.
+
+## Installation 
+N/A
+
+## Usage
+Click the main Generate Password button and answer the prompts.
+
+## Credits
+N/A
+No collaborators.
+
+## License
+Please refer to the LICENSE in the repo.
+
+## Badges
+N/A
+
+## Features
+N/A
+
+## How to Contribute
+N/A
+
+## Tests
+N/A%     
--- script.js
+++ script.js
@@ -1,3 +1,34 @@
+// REQUIREMENTS
+// User Story
+// AS AN employee with access to sensitive data
+// I WANT to randomly generate a password that meets certain criteria
+// SO THAT I can create a strong password that provides greater security
+// Acceptance Criteria
+// GIVEN I need a new, secure password
+// WHEN I click the button to generate a password
+// THEN I am presented with a series of prompts for password criteria
+
+// 1) GET user CRITERIA
+
+// 2) Build a string, charSet, holding the possible characters the user wants
+
+// 3) GET PASSWORD LENGTH
+// What length do you want for your new password (8-128? User inputs length.
+
+// 4) GENERATE PASSWORD, newPswd from charSet
+// for let i=0, i<length, i++ {
+//   newPswd[i] = randomPick from charSet;
+
+// 5) DISPLAY newPswd on screen
+
+// WHEN prompted for password criteria
+// THEN I select which criteria to include in the password
+// WHEN prompted for the length of the password
+// THEN I choose a length of at least 8 characters and no more than 128 characters
+// WHEN asked for character types to include in the password
+// THEN I confirm whether or not to include lowercase, uppercase, numeric, and/or special characters 
+
+
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
@@ -7,20 +38,15 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
 
-
-
-
-
-/*Check all that you want to apply to your new password</h3>
-
-/* X Lowercase letters a,b,c,d,e,f,g,h,I,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z
+/*When user clicks button, ask them for length and ask them to
+Check all that they want to apply to the new password</h3>
+X Lowercase letters a,b,c,d,e,f,g,h,I,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z
 X Uppercase letters A,B,C,DD,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z
 X Numeric 0,1,2,3,4,5,6,7,8,9
 X Special Characters !"#$%&'()*+,-./:;<=>?@[\]^_`{|}~"
@@ -43,15 +69,19 @@ function generatePassword() {
 
 
   // Ask user to input length of the password
-  function askpasswordlength() {
+  function askforpasswordlength() {
     var passwordLength = prompt("How long would you like your password? Must be 8-128 in length");
-    length = parseInt(passwordLength);
+    if (passwordLength = null) {
+      length = 8;
+    } else {
+      length = parseInt(passwordLength);
+    }
     validatelength();
   }
 
 
   // Ask user to choose what type of characters they want for password
-  function askcharset() {
+  function askforcharset() {
     // WHEN asked for character types to include in the password
     // THEN I confirm whether or not to include lowercase, uppercase, numeric, and/or special characters
     while (!validcharset) {
@@ -97,17 +127,18 @@ function generatePassword() {
 
   function buildpassword() {
     // Generate the password
-    for (var i = 0; i < length; i++) {
+    console.log("In buildpasswordd");
+    for (let i = 0; i < length; i++) {
       var randomIndex = Math.floor(Math.random() * charset.length);
       password += charset.charAt(randomIndex);
       console.log("password is", password);
     }
   }
 
-  // Call Functions to generate new password
-  askpasswordlength();
+  // generatePassword calls inner-Functions to generate new password
+  askforpasswordlength();
   validatelength();
-  askcharset();
+  askforcharset();
   determinecharset();
   buildpassword();
   return password;
